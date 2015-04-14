ffc3a2d881281f20a67dd08b2a893383a44b1166
Craig Versek <cversek@gmail.com>
2015-04-13 21:09:04 -0400

    added a bit of error handling and example output to the docstring

--- push-skeleton.py
+++ push-skeleton.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+"""  OPK push skeleton model code for Python.
+     Here are some test results:
+     $ echo "crap" | ./push -v -f junk.asc
+     $ cat junk.asc
+     PUSHING: crap
+     $ echo "crap" | ./push -v -f "junk/.asc"
+     ERROR: [Errno 2] No such file or directory: 'junk/.asc'
+"""
 import sys, time
 import argparse
 
-LOGFILE = sys.stderr
+LOGFILE     = sys.stderr
 IN_STREAM   = sys.stdin
 OUT_STREAM  = sys.stdout
 
@@ -13,8 +21,6 @@ parser.add_argument("-f","--outfile", help="path to optional output file")
 parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
 
 args = parser.parse_args()
-
-
     
 try:
     if args.outfile:
@@ -24,6 +30,8 @@ try:
         if args.verbose:
             OUT_STREAM.write("PUSHING: ")
         OUT_STREAM.write(line)
+except Exception, exc:
+    LOGFILE.write("ERROR: %s" % exc)
 finally:
     LOGFILE.close()
     IN_STREAM.close()
