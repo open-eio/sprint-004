9ce0d25027e37f9c7311a891b3188bfb67e31b9e
Don Blair <donblair@gmail.com>
2015-04-13 20:27:05 -0400

    update

---
+++ phant1.py
@@ -0,0 +1,37 @@
+import urllib2
+from random import randint
+import time 
+
+# replace these keys with those appropriate to your feed on data.sparkfun.com
+#publicKey='9JyZ00pQg7iDyOYaWDrv'
+#privateKey='xz4y55b8R1tbm5owZb47'
+
+#publicKey='xR0W4ZM6vpCO4waYNRzA'
+#privateKey='ZaevwqAKzrS2WmJoE89n'
+
+publicKey='AJjpo34AYQc1d401qA41'
+privateKey='rzqrD4bYKMiznp0zqBpz'
+
+while 1:
+
+    # create a random number (in future, could be a measurement)
+    r = randint(1,10)/10.
+
+    print r
+
+    # here, we're calling our parameter 'rando':
+
+    temp = randint(1,1000)/10.
+    conductivity = randint(1,1000)/10.
+    depth = randint(1,1000)/10.
+
+    sentence = 'https://data.sparkfun.com/input/'+str(publicKey)+'?private_key='+str(privateKey)+'&temp='+str(r)+'&conductivity='+str(conductivity)+'&depth='+str(depth)
+
+    print sentence
+
+    url_response = urllib2.urlopen(sentence)
+
+    print url_response
+
+    time.sleep(5) 
+
